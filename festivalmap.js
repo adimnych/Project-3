7c77ab816c2d54dc4cb37f804f181e8cb6caee52
adimnych <andydimnych@gmail.com>
2017-07-05 05:05:44 -0500

    updated the locations of the map with lat and long

--- festivalmap.js
+++ festivalmap.js
@@ -1,33 +1,32 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="viewport" content="initial-scale=1.0">
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="normalize.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <title>Top 10 Electronic Music Festivals in the United States</title>
-    <style>
-      #map {
-        height: 100%;
-      }
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="map"></div>
-		
-    <script>
-      var map;
-      function Map() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 40, lng: -98},
-          zoom: 10
-        });
-      }
+function (newMap) {
+  var center = {lat: 40, lng: -98}, // this is where my map will be centered at
+  var map = new google.maps.Map(document.getElementById('google_api'),{zoom: 5, center: center});
+   var festivals = [
+
+     ['Lollapalooza', 41.8828° N, 87.6189° W,'<h1 style="font-size: 22px; margin-top:0px">Lollapalooza</h1><p style="font-size: 10px">August 3-6, 2017.</p>''],
+     ['Ultra Music Festival', 25.7617° N, 80.1918° W,'<h1 style="font-size: 22px; margin-top:0px">Ultra Music Festival</h1><p style="font-size: 10px">August 3-6, 2017.</p>''],
+     ['Burning Man', 40.9107° N, 119.0560° W,'<h1 style="font-size: 22px; margin-top:0px">Burning Man</h1><p style="font-size: 10px">August 3-6, 2017.</p>''],
+     ['Electric Daisy Carnival Las Vegas', 36.1699° N, 115.1398° W,'<h1 style="font-size: 22px; margin-top:0px">Electric Daisy Carnival Las Vegas</h1><p style="font-size: 10px">August 3-6, 2017.</p>''],
+     ['Electric Forest', 43.5072° N, 86.3476° W,'<h1 style="font-size: 22px; margin-top:0px">Electric Forest</h1><p style="font-size: 10px">August 3-6, 2017.</p>''],
+     ['Bannaroo', 41.8828° N, 87.6189° W,'<h1 style="font-size: 22px; margin-top:0px">Bannaroo</h1><p style="font-size: 10px">August 3-6, 2017.</p>''],
+     ['Summerset', 45.1244° N, 92.6735° W,'<h1 style="font-size: 22px; margin-top:0px">Summerset</h1><p style="font-size: 10px">August 3-6, 2017.</p>''],
+     ['Electric Zoo', 40.7932° N, 73.9213° W,'<h1 style="font-size: 22px; margin-top:0px">Electric Zoo</h1><p style="font-size: 10px">August 3-6, 2017.</p>''],
+     ['Coachella', 33.6809° N, 116.2372° W,'<h1 style="font-size: 22px; margin-top:0px">Coachella</h1><p style="font-size: 10px">August 3-6, 2017.</p>''],
+     ['Governors Ball', 41.7932° N, 74.9213°  W,'<h1 style="font-size: 22px; margin-top:0px">Governors Ball</h1><p style="font-size: 10px">August 3-6, 2017.</p>''],
+
+
+
+   ] 
+
+
+
+  }
+
+
+
+
+}
+  
     </script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAM3lx_-uty02gVUQga-rze1vL-jdBIpiw-callback=Map"
     async defer></script>
